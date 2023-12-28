438ef99a4da43955d64d5866ade2fde677b17352
jucsmartin <julianacmsantos@gmail.com>
2023-12-28 12:31:16 -0300

    1st commit: personalization

--- seoConfig/index.js
+++ seoConfig/index.js
@@ -1,12 +1,12 @@
 export default {
-  title: "Logspot - Opensource changelog template",
+  title: "Juliana's Portfolio",
   description:
-    "Logspot is an open source change log template made with Nuxt, Vue, and Tailwindcss.",
+    "Here you will get to know the professional.",
   og: {
-    title: "Logspot - Opensource changelog template",
+    title: "Juliana's Portfolio",
     description:
-      "Logspot is an open source change log template made with Nuxt, Vue, and Tailwindcss.",
-    image: "https://logspot.vercel.app/logspot-banner.png",
-    url: "https://logspot.vercel.app",
+      "Here you will get to know the professional.",
+    image: "/profile-picture.png",
+    url: "https://www.linkedin.com/in/juliana-martinez-3b9170114/",
   },
 };
